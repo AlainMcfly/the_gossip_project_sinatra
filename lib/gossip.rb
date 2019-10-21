845618166b46871eccd2436b0586a55ee697d81a
AlainMcfly <alain.mais@free.fr>
2019-10-21 19:26:43 +0200

    Sinatra_first

---
+++ lib/gossip.rb
@@ -0,0 +1,15 @@
+require 'pry'
+require 'csv'
+class Gossip
+    
+    def initialize(author, content)
+        @author = author
+        @content = content
+    end
+    
+    def save
+  CSV.open("./db/gossip.csv", "ab") do |csv|
+    csv << [@author, @content]
+  end
+end
+end
